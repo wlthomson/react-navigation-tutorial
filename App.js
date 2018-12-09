bf6139690b255a4b506cf39a9782665f8f30fe69
wlthomson <will.tho@outlook.com>
2018-12-10 10:06:28 +1300

    Add buttons to goBack, navigate from DetailsScreen to HomeScreen

--- App.js
+++ App.js
@@ -31,6 +31,14 @@ class DetailsScreen extends React.Component {
           title="Go to Details... again"
           onPress={() => this.props.navigation.push('Details')}
         />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
       </View>
     );
   }
