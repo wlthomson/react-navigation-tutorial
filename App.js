ca2e0d055e7f044b74f148b0e981c0e95d623924
wlthomson <will.tho@outlook.com>
2018-12-10 10:03:30 +1300

    Add button to navigate to new DetailsScreen from DetailsScreen

--- App.js
+++ App.js
@@ -27,6 +27,10 @@ class DetailsScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details')}
+        />
       </View>
     );
   }
