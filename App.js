9a2350c3ee901ee0f89d32e75731fc263e7f6d5c
wlthomson <will.tho@outlook.com>
2018-12-10 09:46:40 +1300

    Refactor basic navigation

--- App.js
+++ App.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
 class HomeScreen extends React.Component {
   render() {
     return (
@@ -11,11 +23,3 @@ class HomeScreen extends React.Component {
     );
   }
 }
-
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  }
-});
-
-export default createAppContainer(AppNavigator);
