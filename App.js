302a81e60691f355732d69757f3f330c5f70247b
wlthomson <will.tho@outlook.com>
2018-12-10 09:53:32 +1300

    Add additional route

--- App.js
+++ App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-const AppNavigator = createStackNavigator({
-  Home: HomeScreen
-});
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: 'Home'
+  }
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
@@ -19,6 +25,16 @@ class HomeScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
       </View>
     );
   }
