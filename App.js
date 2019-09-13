5e3ec99b1a255b1796c2c3d00dc0338b32def65f
Will Thomson <will.tho@outlook.com>
2019-09-14 00:13:27 +1200

    Prettify js files

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import { Button, View, Text } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 export default class App extends React.Component {
   render() {
@@ -9,23 +9,23 @@ export default class App extends React.Component {
 }
 
 class HomeScreen extends React.Component {
-  static navigationOptions = { title: 'Home' }
+  static navigationOptions = { title: "Home" };
 
   render() {
     const { navigation } = this.props;
     const { navigate } = navigation;
 
     const navigateDetails = () => {
-      navigate('Details', {
-        someDetail: 'This is a detail.',
-        anotherDetail: 'This is another detail.'
+      navigate("Details", {
+        someDetail: "This is a detail.",
+        anotherDetail: "This is another detail."
       });
-    }
+    };
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
-        <Button title="Go to Details" onPress={navigateDetails}/>
+        <Button title="Go to Details" onPress={navigateDetails} />
       </View>
     );
   }
@@ -33,44 +33,45 @@ class HomeScreen extends React.Component {
 
 class DetailsScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
-    const { getParam } = navigation
+    const { getParam } = navigation;
     return {
-      title: getParam('otherParam', 'A Nested Details Screen')
-    }
-  }
+      title: getParam("otherParam", "A Nested Details Screen")
+    };
+  };
 
   render() {
     const { navigation } = this.props;
     const { navigate, push, getParam, goBack, setParams } = navigation;
 
-    const someDetail = getParam('someDetail', 'This is not a detail.');
-    const anotherDetail = getParam('anotherDetail', 'This is not a detail.');
+    const someDetail = getParam("someDetail", "This is not a detail.");
+    const anotherDetail = getParam("anotherDetail", "This is not a detail.");
 
-    const navigateHome = () => navigate('Home')
-    const pushDetails = () => push('Details', {
-      someDetail: someDetail.slice().concat(' Again.'),
-      anotherDetail: anotherDetail.slice().concat(' Again.')
-    })
-    const goBack = () => goBack()
-    const updateTitle = () => setParams({ otherParam: 'Updated!' })
+    const navigateHome = () => navigate("Home");
+    const pushDetails = () =>
+      push("Details", {
+        someDetail: someDetail.slice().concat(" Again."),
+        anotherDetail: anotherDetail.slice().concat(" Again.")
+      });
+    const navigateBack = () => goBack();
+    const updateTitle = () => setParams({ otherParam: "Updated!" });
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
         <Text>{JSON.stringify(someDetail)}</Text>
         <Text>{JSON.stringify(anotherDetail)}</Text>
-        <Button title="Go to Home" onPress={navigateHome}/>
-        <Button title="Go to Details... again" onPress={pushDetails}/>
-        <Button title="Update the title" onPress={updateTitle}/>
-        <Button title="Go back" onPress={goBack}/>
+        <Button title="Go to Home" onPress={navigateHome} />
+        <Button title="Go to Details... again" onPress={pushDetails} />
+        <Button title="Update the title" onPress={updateTitle} />
+        <Button title="Go back" onPress={navigateBack} />
       </View>
     );
   }
 }
 
 const AppNavigator = createStackNavigator(
-  {
-    Home: HomeScreen,
+  { Home: HomeScreen, Details: DetailsScreen },
+  { initialRouteName: "Home" }
     Details: DetailsScreen
   },
   {
